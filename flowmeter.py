d36a958348f6ae61b6107729a397159e5b3acd08
David Meunier <davidmeunier@gmail.com>
2018-06-10 22:40:57 -0400

    Functional version
    -Gets permissions from iot service
    -Reads device config
    -Supports multiple flowmeters
    -Checks for updates on startup

--- flowmeter.py
+++ flowmeter.py
@@ -1,5 +1,6 @@
-import RPi.GPIO as GPIO
+import RPIO
 import sys
+import threading
 
 class FlowMeter:
     
@@ -7,32 +8,58 @@ class FlowMeter:
         self.pin = pin
         self.count = 0
         self.end_timeout_ms = 1000
-        self.gpio_edge_event = GPIO.FALLING
-        self.update_interval = 50
+        self.gpio_edge_event = 'falling'
+        self.update_interval = 20
         self.monitoring = False
         self.flow_started = False
+        self.start_event = None
+        self.update_event = None
+        self.end_event = None
+        self.end_timer = threading.Timer(self.end_timeout_ms/1000, self.timer_callback)
+        self.flow_lock = threading.Lock()
+
+    def gpio_callback(self, gpio_id, value):
+        self.flow_lock.acquire()
+        try:
+            self.count+=1
+            if not self.flow_started:
+                self.flow_started = True
+                if self.start_event:
+                    self.start_event(self)
+            elif self.update_event and self.count % self.update_interval == 0:
+                self.update_event(self)
+            #reset timer
+            self.end_timer.cancel()
+            self.end_timer = threading.Timer(self.end_timeout_ms/1000, self.timer_callback)
+            self.end_timer.start()
+        finally:
+            self.flow_lock.release()
+
+    def timer_callback(self):
+        self.flow_lock.acquire()
+        try:
+            self.flow_started = False
+            if self.end_event:
+                self.end_event(self)
+            self.count = 0
+        finally:
+            self.flow_lock.release()
     
     def monitor(self, start, update, end):
         self.monitoring = True;
         self.count = 0
         self.flow_started = False
-        while (self.monitoring):
-            edge = GPIO.wait_for_edge(self.pin, self.gpio_edge_event, timeout=self.end_timeout_ms)
-            if edge:
-                self.count+=1
-                if not self.flow_started:
-                    self.flow_started = True
-                    if start:
-                        start(self) #signal start event
-                elif update and self.count % self.update_interval == 0:
-                    update(self)
-            elif self.flow_started:
-                self.flow_started = False
-                if end:
-                    end(self)
-                self.count = 0
-        
+        self.start_event = start
+        self.update_event = update
+        self.end_event = end
+        RPIO.add_interrupt_callback(self.pin,
+                                    self.gpio_callback,
+                                    edge=self.gpio_edge_event,
+                                    threaded_callback=True)
+        RPIO.wait_for_interrupts()
+                
     def stop(self):
+        RPIO.stop_waiting_for_interrupts()
         self.flow_started = False
         self.monitoring = False
 
--- piotflow.py
+++ piotflow.py
@@ -1,11 +1,20 @@
-import RPi.GPIO as GPIO
+#!/usr/bin/python
+
+import RPIO
 import sys
 from flowmeter import FlowMeter
 import thread
 import boto3
 import requests
+import threading
 import json
 import getopt
+import time
+from distutils.version import StrictVersion
+import signal
+import zipfile
+
+version = StrictVersion("1.0")
 
 def get_serial():
   # Extract serial from cpuinfo file
@@ -59,67 +68,138 @@ for opt, arg in opts:
 #TODO make sure all arguments are there
 #TODO read table name from command line
 #TODO get data endpoint from cloud
-#TODO get root topic from cloud
 
 print "I'm using the '%s' certificate and '%s' key to get permissions from '%s'" % (certificate, private_key, iot_credentials_endpoint)
 
-#Get this device's registered flowmeter configurations from the cloud
-r = requests.get('https://%s:443/role-aliases/%s/credentials' % (iot_credentials_endpoint, role_alias),
-                 cert=(certificate, private_key),
-                 headers={'x-amzn-iot-thingname': my_name})
+#Get access keys
+r = None
+connect_time = 0
+while not r and connect_time < 60:
+  try: 
+    r = requests.get('https://%s:443/role-aliases/%s/credentials' % (iot_credentials_endpoint, role_alias),
+                     cert=(certificate, private_key),
+                     headers={'x-amzn-iot-thingname': my_name})
+  except requests.exceptions.ConnectionError:
+    time.sleep(5)
+    connect_time += 5
 
-#TODO: validate response code
+if not r:
+  print "Failed to connect after 60 seconds. aborting. " + str(connect_time)
+  sys.exit(1)
+
+if r.status_code != 200:
+  print "Error getting permissions: " + str(r.status_code)
+  sys.exit(1)
+
 creds = r.json()
+
+#Get this device's flowmeter configurations from the cloud
+
 dynamodb = boto3.client('dynamodb',
                         aws_access_key_id=creds['credentials']['accessKeyId'],
                         aws_secret_access_key=creds['credentials']['secretAccessKey'],
                         aws_session_token=creds['credentials']['sessionToken'])
 
 
-item = dynamodb.get_item(TableName='devices',
+item_response = dynamodb.get_item(TableName='devices',
                          Key={'device_id':{'S': my_name}})
-#TODO validate
-print item
+
+print item_response
+
+if not 'Item' in item_response:
+  print "Device not found: " + my_name  
+  sys.exit(1)
+
+item = item_response['Item']
+
+#Check version
+if not 'piotflow_version' in item:
+  print 'No version info found'
+else:
+  latest_version = item['piotflow_version']['S']
+  print latest_version
+  if StrictVersion(latest_version) > version:
+    print 'Downloading new version from ' + item['piotflow_url']['S']
+	
+    r = requests.get(item['piotflow_url']['S'], stream=True)
+    #TODO: Avoid hardcoding this
+    path = '/usr/sbin/piotflow/install.zip'
+    with open(path, 'wb') as fd:
+      for chunk in r.iter_content(chunk_size=300):
+        fd.write(chunk)
+      fd.flush()
+    zip_ref = zipfile.ZipFile(path, 'r')
+    zip_ref.extractall('/usr/sbin/piotflow')
+    zip_ref.close()
+    sys.exit(2)    
+
+if not 'configuration' in item_response['Item']:
+  print "No configuration for " + my_name
+  sys.exit(1)
 
 
-#TODO actually use device config for pin
-#TODO support n flow meters
+device_configs = json.loads(item_response['Item']['configuration']['S'])['flowmeters']
+#[{'pin': 4, 'topic':flow_topic+"/0", 'id':my_name+"/0"},
+#{'pin': 14, 'topic':flow_topic+"/1", 'id':my_name+"/1"},
+#{'pin': 15, 'topic':flow_topic+"/2", 'id':my_name+"/2"}]
 
-
-#setup GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 #setup AWS IOT
 
 iot = boto3.client('iot-data', endpoint_url=iot_data_endpoint)
-
-#TODO: Think about topic naming, or look into using device shadow
-flow_topic = 'flow'
+flowmeter_configs = {}
 
 def flow_started(flowmeter):
+    cfg = flowmeter_configs[flowmeter]
     #Nothing really needs to be done here
-    print "Flow started"
+    print "Flow started on " + cfg['id']
 
 def flow_update(flowmeter):
-    message = b'{"flowmeter_id":"%s/0", "cumulative_flow":%d, "flowing":true}'%(my_name, flowmeter.count)
-    iot.publish(topic=flow_topic,
+    cfg = flowmeter_configs[flowmeter]
+    message = b'{"flowmeter_id":"%s", "cumulative_flow":%d, "flowing":true}'%(cfg['id'], flowmeter.count)
+    iot.publish(topic=cfg['topic'],
     qos=0, #We dont really need high QoS here as we'll be sending a bunch of updates 
     payload=message)
     print "update: " + message
 
 def flow_stopped(flowmeter):
-    message = b'{"flowmeter_id":"%s/0", "cumulative_flow":%d, "flowing":false}'%(my_name, flowmeter.count)
-    iot.publish(topic=flow_topic,
+    cfg = flowmeter_configs[flowmeter]
+    message = b'{"flowmeter_id":"%s", "cumulative_flow":%d, "flowing":false}'%(cfg['id'], flowmeter.count)
+    iot.publish(topic=cfg['topic'],
     qos=1, #Use a higher QoS since this essetially sets the next stable state. Maybe even QoS level 2 should be used. 
     payload=message)
     print "Flow stopped: " + message
 
+def monitor(flowmeter):
+    cfg = flowmeter_configs[flowmeter]
+    print "Monitoring " + cfg['id']
+    flowmeter.monitor(flow_started, flow_update, flow_stopped)
 
-flowmeter = FlowMeter(4)
+#setup GPIO and flow meters
+#TODO actually use device config for pin and topic
+RPIO.setmode(RPIO.BCM)
+threads = []
+for cfg in device_configs:
+    print "Settting up flowmeter %s on pin %d, publishing to %s" % (cfg['id'], cfg['pin'], cfg['topic']) 
+    RPIO.setup(cfg['pin'], RPIO.IN, pull_up_down=RPIO.PUD_UP)
+    flowmeter = FlowMeter(cfg['pin'])
+    flowmeter_configs[flowmeter] = cfg
+    t = threading.Thread(target=monitor, args=(flowmeter,))
+    threads.append(t)
+    t.start()
+
+running=True
+
+def receive_signal(signum, stack):
+    running = False
+    print "Signal received"
+	
+signal.signal(signal.SIGUSR1, receive_signal)
 
 try:
-    print "Monitoring..."
-    flowmeter.monitor(flow_started, flow_update, flow_stopped)
+   while(running):
+     time.sleep(5)
 finally:
-    GPIO.cleanup()
+    for flowmeter in flowmeter_configs:
+        flowmeter.stop()
+    RPIO.cleanup()
